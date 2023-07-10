dae0f08740eaadd4aa0127e97c1239200a8d2026
Danusshkumar <mdanusshkumar@gmail.com>
2023-07-10 21:00:59 +0530

    Debug updated

--- phishingLinkServer/settings.py
+++ phishingLinkServer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--wo+t1dba1$%nm%p996*=8m_9c^_xv&h-*9#l@fkszh30oe59='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
